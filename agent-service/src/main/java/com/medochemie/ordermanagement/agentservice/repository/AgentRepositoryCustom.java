4884e6aed175aff2d631e226ea1806694c697978
solomon <62584475+solomonfy@users.noreply.github.com>
2022-03-23 00:33:21 -0400

    Delete agent-service directory

--- agent-service/src/main/java/com/medochemie/ordermanagement/agentservice/repository/AgentRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.medochemie.ordermanagement.agentservice.repository;
-
-import com.medochemie.ordermanagement.agentservice.entity.Agent;
-
-public interface AgentRepositoryCustom {
-    long updateAgent(Agent agent);
-}
